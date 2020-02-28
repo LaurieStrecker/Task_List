f965987c3f3292186154f3f617cacecfeb343b2e
LahRee <Laurie.Strecker@gmail.com>
2020-02-28 14:40:21 -0800

    updates to all code, especially styling

--- run.js
+++ run.js
@@ -1,25 +1,28 @@
 //Event Listeners
 $('button').click(addTask);
-$('input').keypress(function(event){
-  if(event.which == 13){
+$('input').keypress(function(event) {
+  if(event.which == 13) {
     addTask();
   }
 });
-//Delete 
+
+//Delete
 $(document).on('click', '#delete', removeTask);
 
 function addTask() {
   var task = $('#newtask').val();
 
-  $('#tasks').append('<div>' + task + '<span id="delete">X</span></div>');
-  
+  $('#tasks').append('<div class="task-div">' + task + '<span id="delete">💀</span></div>');
+
   //Clear the box
   $('#newtask').val(" ");
-
 }
 
-function removeTask() {
-  $(this).closest('div').remove();
-}
+  function removeTask() {
+
+    $(this).closest('div').remove();
+  }
+
+
 
 
